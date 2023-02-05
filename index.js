c1839c7dea09076993df4631220c8b65c62a903a
Beatriz Martinez Perez <trism460@gmail.com>
2023-02-05 13:27:45 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+// Auto execute this function when the server is started
+(async () => {
+    // Create constants & variables
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Open the specified page and take a screenshot of it
+    await page.goto('https://www.utags.edu.mx/');
+    await page.screenshot({ path: 'screenshots/image.jpg'});
+    await browser.close();
+    console.log('Done');
+})();
+ 
