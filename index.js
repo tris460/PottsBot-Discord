dceff9b23e11d81b89c905c76632fc135d44fde9
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-03-14 18:25:34 -0700

    Generate PDF status - incomplete

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 'use strict';
 
 const UtaScrapper = require('./Functions/utaScraper');
+const websiteStatus = require('./Functions/websiteStatus');
 const { generateQR } = require('./Functions/generateQR');
 const { getFortune } = require('./Functions/fortune');
 
 const urlToQR = 'https://www.youtube.com/watch?v=kF-wqxZPGwA'; // URL to convert to QR code
 const QRFileName = 'qr.jpg'; // Image's name for the QR
+const urlToScan = 'https://github.com';
 
 // Auto execute this function when the server is started
 (async () => {
@@ -17,7 +19,10 @@ const QRFileName = 'qr.jpg'; // Image's name for the QR
 
   // await generateQR(urlToQR, QRFileName);
   
-  let fortune = getFortune('shellFortune');
-  console.log(fortune);
+  // let fortune = getFortune('shellFortune');
+  // console.log(fortune);
+
+  const webStatus = new websiteStatus();
+  webStatus.getStatus(urlToScan);
 })();
  
